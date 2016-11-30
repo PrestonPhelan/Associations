0b5baddd906d4e58be8e0ac296b195b909f45691
App Academy Student <student@appacademy.io>
2016-11-30 11:35:15 -0800

    Added instructor and prerequisite associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,23 @@ class Course < ActiveRecord::Base
   primary_key: :id,
   foreign_key: :course_id,
   class_name: :Enrollment
+
+  has_many :prerequisites,
+  primary_key: :prereq_id,
+  foreign_key: :id,
+  class_name: :Course
+
+  ##belongs_to :prereq,
+  ##primary_key: :id,
+  ##foreign_key: :prereq_id,
+  ##class_name: :Course
+
+  #Look in Course for "foreign_key" that matches our "primary_key"
+
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
+
+  #Look in User for "primary_key" that mathes our "foreign_key"
 end
